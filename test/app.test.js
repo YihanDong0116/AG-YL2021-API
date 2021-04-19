6ff804835adab03d8f0ad77012042a1fbc5527df
maxplumley <maxplumley@users.noreply.github.com>
2021-04-19 18:47:30 +1000

    Update app.test.js

---
+++ test/app.test.js
@@ -0,0 +1,14 @@
+const request = require("supertest");
+const app = require("../src/app");
+
+describe("Test paths", () => {
+  test("Given valid request when GET / then return OK", async () => {
+    const response = await request(app).get("/");
+    expect(response.statusCode).toBe(200);
+  });
+
+  test("Given valid request when GET /health then return OK", async () => {
+    const response = await request(app).get("/health");
+    expect(response.statusCode).toBe(201);
+  });
+});
