bd53e8b73be2a8ef6dd56831be0308ba65aa8da1
Yifan ZHAO <yifan.zhao4@student.unimelb.edu.au>
2021-04-29 01:52:46 +0800

    added instructions
    - Fix more typos in test file :dizzy_face:
    - Add instruction on swaggerUI page

--- test/app.test.js
+++ test/app.test.js
@@ -12,17 +12,17 @@ describe('Test paths', () => {
     expect(response.statusCode).toBe(200);
   });
 
-  test('Given valid request when GET /users then return OK', async () => {
+  test('Given valid request when GET /courses then return OK', async () => {
     const response = await request(app).get('/courses');
     expect(response.statusCode).toBe(200);
   });
 
-  test('Given valid request when GET /users/correct_user_id then return OK', async () => {
+  test('Given valid request when GET /courses/correct_course_id then return OK', async () => {
     const response = await request(app).get('/courses/correct_course_id');
     expect(response.statusCode).toBe(200);
   });
 
-  test('Given invalid request when GET /users/wrong_id then return 404', async () => {
+  test('Given invalid request when GET /courses/wrong_id then return 404', async () => {
     const response = await request(app).get('/courses/wrong_id');
     expect(response.statusCode).toBe(404);
   });
