982656994b0c7cc1185f7f8cf5375033db3786b1
Yifan ZHAO <yifan.zhao4@student.unimelb.edu.au>
2021-05-12 15:10:43 +0800

    Add getById and test cases

--- src/routes/courses.js
+++ src/routes/courses.js
@@ -1,87 +1,20 @@
 const express = require('express');
-
+const courseloader = require('../util/loadCourses')
 const router = express.Router();
+
 
 /* GET courses listing. */
 router.get('/', (req, res) => {
-  const listObj = [
-    {
-      course_id: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
-      name: 'dijkstra',
-      default_page: '3fa85f64-5717-4562-b3fc-2c963f66afa1',
-      pages: [
-        {
-          id: '3fa85f64-5717-4562-b3fc-2c963f66afa1',
-          next: '3fa85f64-5717-4562-b3fc-2c963f66afa2',
-          prev: '3fa85f64-5717-4562-b3fc-2c963f66afa3',
-        },
-        {
-          id: '3fa85f64-5717-4562-b3fc-2c963f66afa2',
-          next: '3fa85f64-5717-4562-b3fc-2c963f66afa3',
-          prev: '3fa85f64-5717-4562-b3fc-2c963f66afa1',
-        },
-        {
-          id: '3fa85f64-5717-4562-b3fc-2c963f66afa3',
-          next: '3fa85f64-5717-4562-b3fc-2c963f66afa1',
-          prev: '3fa85f64-5717-4562-b3fc-2c963f66afa2',
-        },
-      ],
-    },
-    {
-      course_id: '3fa85f64-5717-4562-b3fc-2c963f66afb6',
-      name: 'dijkstra_2',
-      default_page: '3fa85f64-5717-4562-b3fc-2c963f66afb1',
-      pages: [
-        {
-          id: '3fa85f64-5717-4562-b3fc-2c963f66afb1',
-          next: '3fa85f64-5717-4562-b3fc-2c963f66afb2',
-          prev: '3fa85f64-5717-4562-b3fc-2c963f66afb3',
-        },
-        {
-          id: '3fa85f64-5717-4562-b3fc-2c963f66afb2',
-          next: '3fa85f64-5717-4562-b3fc-2c963f66afb3',
-          prev: '3fa85f64-5717-4562-b3fc-2c963f66afb1',
-        },
-        {
-          id: '3fa85f64-5717-4562-b3fc-2c963f66afb3',
-          next: '3fa85f64-5717-4562-b3fc-2c963f66afb1',
-          prev: '3fa85f64-5717-4562-b3fc-2c963f66afb2',
-        },
-      ],
-    },
-  ];
-  res.json(listObj);
-  // res.send(listObj);
+  res.json(courseloader.courseData)
 });
 
-/* GET specific course. */
 
+/* GET specific course. */
 router.get('/:course_id', (req, res) => {
-  if (req.params.course_id === 'correct_course_id') {
-    const coursePages = {
-      course_id: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
-      name: 'dijkstra',
-      default_page: '3fa85f64-5717-4562-b3fc-2c963f66afa1',
-      pages: [
-        {
-          id: '3fa85f64-5717-4562-b3fc-2c963f66afa1',
-          next: '3fa85f64-5717-4562-b3fc-2c963f66afa2',
-          prev: '3fa85f64-5717-4562-b3fc-2c963f66afa3',
-        },
-        {
-          id: '3fa85f64-5717-4562-b3fc-2c963f66afa2',
-          next: '3fa85f64-5717-4562-b3fc-2c963f66afa3',
-          prev: '3fa85f64-5717-4562-b3fc-2c963f66afa1',
-        },
-        {
-          id: '3fa85f64-5717-4562-b3fc-2c963f66afa3',
-          next: '3fa85f64-5717-4562-b3fc-2c963f66afa1',
-          prev: '3fa85f64-5717-4562-b3fc-2c963f66afa2',
-        },
-      ],
-    };
-    res.json(coursePages);
-    // res.send(coursePages);
+  console.log('parameters type :' + typeof(req.params.course_id))
+  if (courseloader.hasId(req.params.course_id) == true) {
+    res.status(200);
+    res.json(courseloader.getById(req.params.course_id));
   } else {
     res.status(404);
     res.send('course_id does not exist!');
--- src/util/loadCourses.js
+++ src/util/loadCourses.js
@@ -4,16 +4,31 @@ const fs = require('fs')
 class courseLoader {
   constructor() {
     var data = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'algorithms.json'), 'utf8'));
-    // this.courseData = {'sample': 1};
     this.courseData = data;
   }
 
-  getByName(name) {
-    return this.courseData.find((course) => name == course.name)
+  /* get a course by name */
+
+  // Currently I'm not so sure if we need this function, will just leave it here
+
+  // getByName(name) {
+  //   return this.courseData.find((course) => name === course.name)
+  // }
+
+  /* check existence of a course with certain id */
+  hasId(id) {
+    let found = false;
+    this.courseData.forEach((element) => {
+      if (element.id == id) found = true;
+    });
+    return found;
   }
 
+  /* get a course by id */
   getById(id) {
-    return this.courseData.find((course) => id == course.id)
+    let res = this.courseData.find((course) => id == course.id);
+    if (res) return res;
+    else alert('No courses found by that ID');
   }
 }
 
