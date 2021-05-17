3fecf023d649b8db1c328771b7297aa33a0e07e7
Yifan ZHAO <yifan.zhao4@student.unimelb.edu.au>
2021-05-17 11:11:40 +0800

    add pages orders

--- src/courses/course1/page1.js
+++ src/courses/course1/page1.js
@@ -20,4 +20,5 @@ module.exports = {
       check: (inputs) => inputs.id === 'red',
     },
   ],
+  next: '3fa85f64-5717-4562-b3fc-2c963f66afa2',
 };
--- src/courses/course1/page2.js
+++ src/courses/course1/page2.js
@@ -15,4 +15,5 @@ module.exports = {
       content: 'viva la dijkstra',
     },
   ],
+  previous: '3fa85f64-5717-4562-b3fc-2c963f66afa1',
 };
