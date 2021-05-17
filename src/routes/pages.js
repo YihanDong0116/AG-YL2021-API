c27fb2d8c488f8db59b79e4f0942c867d7d1bc8b
LIHANGT <lihangt@student.unimelb.edu.au>
2021-05-17 16:16:28 +1000

    Complete Get page by ID

--- src/routes/pages.js
+++ src/routes/pages.js
@@ -1,34 +1,13 @@
 const express = require('express');
+const pageloader = require('../util/loadPages');
 
 const router = express.Router();
 
 /* GET specific page. */
 router.get('/:page_id', (req, res) => {
-  if (req.params.page_id === 'correct_page_id') {
-    const page = {
-      id: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
-      type: 'info',
-      sections: [
-        {
-          type: 'text',
-          content: 'viva la dijkstra',
-        },
-        {
-          type: 'image',
-          content: 'https://i.redd.it/tfugj4n3l6ez.png',
-        },
-        {
-          type: 'animation',
-          content: 'https://www.youtube.com/watch?v=k6Es9Qrh4Ao',
-        },
-      ],
-      problem: {
-        type: 'code',
-        data: {},
-      },
-    };
-    res.json(page);
-    // res.send(page);
+  if (pageloader.hasPage(req.params.page_id) === true) {
+    res.status(200);
+    res.json(pageloader.getPageById(req.params.page_id));
   } else {
     res.status(404);
     res.send('page_id does not exist!');
---
+++ src/util/loadPages.js
@@ -0,0 +1,50 @@
+const courseList = require('../courses');
+const pagelist = [];
+courseList.forEach((course) => {
+    course.pages.forEach((page) => {
+        pagelist.push(page);
+    });
+});
+
+class PageLoader {
+  constructor() {
+    this.pagelist = pagelist;
+  }
+
+  /* get all pages id and type*/
+  getPages() {
+    const res = [];
+    this.pagelist.forEach((page) => {
+        const summary = {
+            id: page.id,
+            type: page.type,
+            };
+            res.push(summary);
+    });
+    return res;
+  }
+
+  /* check existence of a page with certain id */
+  hasPage(id) {
+    let res = false;
+    this.pagelist.forEach((page) => {
+      if (page.id === id) {
+        res = true;
+      }
+    });
+    return res;
+  }
+
+  /* get a page by id */
+  getPageById(id) {
+    let res = {};
+    this.pagelist.forEach((page) => {
+      if (page.id === id) res = page;
+    });
+    return res;
+  }
+}
+
+const pageloader = new PageLoader();
+
+module.exports = pageloader;
