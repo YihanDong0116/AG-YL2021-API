473aef01ba41ea24c939c8c9e7fdbde5da927b8a
maxplumley <maxplumley@users.noreply.github.com>
2021-10-20 15:04:36 +1100

    Update shortestPathPracticePage1, shortestPathPracticePage1.js, and 2 more files...

--- src/courses/course1/visitNodesBlocklyProblem.js
+++ src/courses/course1/visitNodesBlocklyProblem.js
@@ -1,3 +1,5 @@
+const Graph = require('../../graph/graph');
+
 const initialGraph = {
   width: 400,
   height: 400,
@@ -73,12 +75,57 @@ const initialGraph = {
   ],
 };
 
+const buildGraph = (graph) => {
+  const g = new Graph();
+  g.init(graph.nodes, graph.edges);
+  return g;
+};
+
+const getVar = (def) => {
+  switch (def.type) {
+    case 'simple':
+      return def.value;
+    case 'graph':
+      return buildGraph(def.value);
+    default:
+      throw new Error(`error initializing input for unknown variable type ${def.type}`);
+  }
+};
+
+const generateVars = (initialVars) => ({
+  variables: Object.keys(initialVars)
+    .reduce((acc, curr) => {
+      acc[curr] = getVar(initialVars[curr]);
+      return acc;
+    }, {}),
+  console: [],
+});
+
+const execute = (initialVars, code) => {
+  const execution = generateVars(initialVars);
+  // eslint-disable-next-line no-eval
+  eval(code);
+  return { initial: generateVars(initialVars), executed: execution };
+};
+
+const executeAndGetNodesVisited = (code) => {
+  const execution = execute({ graph: { type: 'graph', value: initialGraph } }, code);
+  return execution.executed.variables.graph.events
+    .filter((e) => e.type === 'visitNode')
+    .reduce((acc, e) => {
+      if (acc.indexOf(e.data.nodeId) === -1) {
+        acc.push(e.data.nodeId);
+      }
+      return acc;
+    }, []);
+};
+
 module.exports = {
   title: 'Putting Those Blocks to Work!',
   type: 'practice',
   problem: {
     type: 'graphBlockly',
-    question: 'Use the provided blocks to build an algorithm that visits each node of the graph below in any order.',
+    question: 'Use the provided blocks to build an algorithm that visits each node of the graph below, in any order.',
     data: {
       initialGraph,
       blocks: [
@@ -103,7 +150,7 @@ module.exports = {
       ],
     },
     hints: [
-      'Drag blocks from the toolbox on the left into the space adjacent, when your happy with your work click run to execute your code',
+      'Drag blocks from the toolbox on the left into the space adjacent, when you\'re happy with your work click run to execute your code',
     ],
     sections: [{
       type: 'graph',
@@ -112,8 +159,18 @@ module.exports = {
   },
   tests: [
     {
-      feedback: 'It looks like you haven\'t selected the correct number of nodes.',
-      check: (inputs) => inputs.events.length === initialGraph.nodes.length,
+      feedback: 'It looks like you haven\'t visited any nodes. Use the blocks to construct an algorithm that visits each node.',
+      check: (data) => {
+        const nodes = executeAndGetNodesVisited(data.code);
+        return nodes.length !== 0;
+      },
+    },
+    {
+      feedback: 'It looks like you haven\'t visited all the nodes. Make sure your algorithm visits each node, in any order.',
+      check: (data) => {
+        const nodes = executeAndGetNodesVisited(data.code);
+        return nodes.length === initialGraph.nodes.length;
+      },
     },
   ],
 };
