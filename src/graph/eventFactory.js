55d17e49f72d5d7b41237741626b45ec9b1661f8
maxplumley <maxplumley@users.noreply.github.com>
2021-10-31 11:20:14 +1100

    Update eventFactory.js

--- src/graph/eventFactory.js
+++ src/graph/eventFactory.js
@@ -4,7 +4,7 @@ const removeEdgeEvent = require('./removeEdgeEvent');
 const removeNodeEvent = require('./removeNodeEvent');
 const visitNodeEvent = require('./visitNodeEvent');
 const traverseEdgeEvent = require('./traverseEdgeEvent');
-const SetDistanceEvent = require('./setDistanceEvent');
+const setDistanceEvent = require('./setDistanceEvent');
 
 const make = (event) => {
   const time = event.time || new Date();
@@ -21,8 +21,8 @@ const make = (event) => {
       return new removeEdgeEvent.RemoveEdgeEvent(event.data, time);
     case removeNodeEvent.TYPE:
       return new removeNodeEvent.RemoveNodeEvent(event.data, time);
-    case SetDistanceEvent.TYPE:
-      return new SetDistanceEvent.SetDistanceEvent(event.data, time);
+    case setDistanceEvent.TYPE:
+      return new setDistanceEvent.SetDistanceEvent(event.data, time);
     default:
       throw new Error(`unknown event type ${event.type}`);
   }
